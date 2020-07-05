594fa0e9ff616ebb7ecccbef852140e556cddded
najuspy <sazeal.sujan@gmail.com>
2020-07-05 12:19:09 +0545

    Updates

---
+++ precache-manifest.e46f87f70272ef45f2da0eec7be763b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8573cf3a7cfa6046220c0bdacf7bc9e",
+    "url": "/cartoon-search/index.html"
+  },
+  {
+    "revision": "fdabaafcc01efcd02250",
+    "url": "/cartoon-search/static/css/main.18718dc8.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/cartoon-search/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cartoon-search/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdabaafcc01efcd02250",
+    "url": "/cartoon-search/static/js/main.aef0d08c.chunk.js"
+  },
+  {
+    "revision": "709a1169d9be257fccda",
+    "url": "/cartoon-search/static/js/runtime-main.7b94fa26.js"
+  }
+]);
